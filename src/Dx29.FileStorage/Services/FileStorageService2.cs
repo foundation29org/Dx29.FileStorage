160d7713bf430fde3872f49bdab6cd15e5fab9e5
Alvaromah <alvaroma@live.com>
2021-12-14 21:05:57 +0100

    Project startup

---
+++ src/Dx29.FileStorage/Services/FileStorageService2.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Dx29.Services
+{
+    public class FileStorageService2
+    {
+        public FileStorageService2(BlobStorage blobStorage)
+        {
+            BlobStorage = blobStorage;
+        }
+
+        public BlobStorage BlobStorage { get; }
+
+        public async Task UploadFileAsync(string userId, string caseId, string filePath, Stream stream)
+        {
+            string container = userId;
+            string path = $"{caseId}/{filePath}";
+            await BlobStorage.UploadStreamAsync(container, path, stream);
+        }
+
+        public async Task<Stream> DownloadFileAsync(string userId, string caseId, string filePath)
+        {
+            string container = userId;
+            string path = $"{caseId}/{filePath}";
+            return await BlobStorage.DownloadStreamAsync(container, path);
+        }
+
+        public async Task DeleteFileAsync(string userId, string caseId, string filePath)
+        {
+            string container = userId;
+            string path = $"{caseId}/{filePath}";
+            await BlobStorage.DeleteBlobAsync(container, path);
+        }
+
+        public async Task MoveFileAsync(string userId, string sourcePath, string targetPath)
+        {
+            string container = userId;
+            await BlobStorage.MoveBlobAsync(container, sourcePath, targetPath);
+        }
+        public async Task MoveFileAsync(string sourceUserId, string targetUserId, string sourcePath, string targetPath)
+        {
+            string sourceContainer = sourceUserId;
+            string targetContainer = targetUserId;
+            await BlobStorage.MoveBlobAsync(sourceContainer, targetContainer, sourcePath, targetPath);
+        }
+
+        public async Task CopyFileAsync(string userId, string sourcePath, string targetPath)
+        {
+            string container = userId;
+            await BlobStorage.CopyBlobAsync(container, sourcePath, targetPath);
+        }
+        public async Task CopyFileAsync(string sourceUserId, string targetUserId, string sourcePath, string targetPath)
+        {
+            string sourceContainer = sourceUserId;
+            string targetContainer = targetUserId;
+            await BlobStorage.CopyBlobAsync(sourceContainer, targetContainer, sourcePath, targetPath);
+        }
+
+        public string CreateFileShare(string userId, string caseId, string filePath)
+        {
+            string container = userId;
+            string path = $"{caseId}/{filePath}";
+            return BlobStorage.CreateBlobShare(container, path, seconds: 5 * 60);
+        }
+    }
+}
---
+++ src/Dx29/Extensions/ObjectExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+using System.Security.Cryptography;
+
+using Newtonsoft.Json;
+
+namespace Dx29
+{
+    static public class ObjectExtensions
+    {
+        static public string Serialize(this object obj, bool indented = true)
+        {
+            return JsonConvert.SerializeObject(obj, indented ? Formatting.Indented : Formatting.None);
+        }
+
+        static public T Deserialize<T>(this string str)
+        {
+            return JsonConvert.DeserializeObject<T>(str);
+        }
+
+        static public string AsString(this byte[] buffer)
+        {
+            return Encoding.UTF8.GetString(buffer);
+        }
+
+        static public string HashString(this string str)
+        {
+            using (var sha = SHA256.Create())
+            {
+                var hash = sha.ComputeHash(Encoding.UTF8.GetBytes(str));
+                return BitConverter.ToString(hash).Replace("-", "").ToLower();
+            }
+        }
+    }
+}
